071e344bd3f998359acada2260a466d3c6608373
0V7TBDB_notebook <hansol1108@naver.com>
2017-05-18 09:14:30 +0900

    나머지가 0일때가 안됨.

--- alego_3_1/alego_3_1/code_edit.c
+++ alego_3_1/alego_3_1/code_edit.c
@@ -11,8 +11,6 @@ int factorial(int N) {//팩토리얼
 int permu(int numberOfOder, int one, int two, int three) {//같은 것이 있는 순열
 	int result = 0;
 
-	if (numberOfOder == 0)
-		numberOfOder++;
 	if (one == 0)
 		one++;
 	if (two == 0)
@@ -40,7 +38,7 @@ int TwoThree(int N, int twocount, int threecount) {
 	}
 
 	//변수가 1과 3으로만 구성된경우
-	while (three_ > 3)
+	while (three_ >= 3)
 	{
 		three_ = three_ - 3;
 		threeCount++;
@@ -53,33 +51,53 @@ int TwoThree(int N, int twocount, int threecount) {
 
 
 void main() {
-	int input;
-	int result = 1;
-	int q, r;
-	while (1) {
 
+	while (1) {
+		int input=0;
+		int result = 1;
+		int q = 0, r = 0, xtra = 0;
 		printf("N을 입력해 주세요: ");
 		scanf("%d", &input);
 		q = input / 5;
 		r = input % 5;
 		printf("\n");
-		switch (r)
+		if (q == 0)
+			result = result + TwoThree(input, 0, 0);
+		else
 		{
-		case 0: 
-			result= result+ TwoThree(input,0,0) + permu((q*2), 0, q, q); // 2 3 | 0
-		case 1:
-			result = result + TwoThree(input, 0, 0) + permu(((q*2)+1),1,q,q); //2 3 | 1
-		case 2:
-			result = result+ TwoThree(input, 0, 0) + permu((q*2)+2,2,q,q)+ permu((q*2)+1,0,q+1,q) ;// 2 3 | 11 + 2 3 | 2
-		case 3:
-			result = result + TwoThree(input, 0, 0) + permu((q * 2) + 3, 3, q, q) + permu((q * 2) + 2, 1, q+1, q)+ permu((q * 2) + 1, 0, q, q+1);// 2 3 | 111, 21 , 3
-		case 4:
-			result = result + TwoThree(input, 0, 0) + permu((q * 2) + 4, 4, q, q) + permu((q * 2) + 2, 0, q + 2, q) + permu((q * 2) + 2, 1, q, q + 1);;// 2 3 | 1111, 22, 31
-		default:
-			break;
+			switch (r)
+			{
+			case 0: // 2 3 | 0 , 11111
+				
+				result = result + TwoThree(input, 0, 0);
+				while (q > 0) {
+					result = result + TwoThree((q * 2)+xtra, xtra, q, q);
+					result++;
+					q--;
+					xtra = xtra + 5;
+				}
+					
+					
+				break;
+			case 1:
+				result = result + TwoThree(input, 0, 0) + permu(((q * 2) + 1), 1, q, q); //2 3 | 1
+				break;
+			case 2:
+				result = result + TwoThree(input, 0, 0) + permu((q * 2) + 2, 2, q, q) + permu((q * 2) + 1, 0, q + 1, q);// 2 3 | 11 + 2 3 | 2
+				break;
+			case 3:
+				result = result + TwoThree(input, 0, 0) + permu((q * 2) + 3, 3, q, q) + permu((q * 2) + 2, 1, q + 1, q) + permu((q * 2) + 1, 0, q, q + 1);// 2 3 | 111, 21 , 3
+				break;
+			case 4:
+				result = result + TwoThree(input, 0, 0) + permu((q * 2) + 4, 4, q, q) + permu((q * 2) + 2, 0, q + 2, q) + permu((q * 2) + 2, 1, q, q + 1);;// 2 3 | 1111, 22, 31
+				break;
+			default:
+				break;
+			}
 		}
+		
 
-		printf("결과: %d", result);
+		printf("결과: %d\n", result);
 
 	}
 	
