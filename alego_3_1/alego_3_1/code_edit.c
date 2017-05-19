4be766d6004bdf1d8f9b6877e507c963a046056b
0V7TBDB_notebook <hansol1108@naver.com>
2017-05-19 10:59:02 +0900

    완성!

--- alego_3_1/alego_3_1/code_edit.c
+++ alego_3_1/alego_3_1/code_edit.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
-int factorial(int N) {//팩토리얼
-	int result = N;
-	while (N > 1) {
+unsigned __int64 factorial(unsigned __int64 N) {//팩토리얼
+	unsigned __int64 result = N;
+	while (N > 1)
+	{
 		result = result*(--N);
 	}
 	return result;
@@ -23,26 +24,31 @@ int permu(int numberOfOder, int one, int two, int three) {//같은 것이 있는 순열
 	return result;
 }
 
-int socket(int N, int one, int two, int three) {//처리하는 함수
+int socket(int n, int one, int two, int three) {//처리하는 함수
 
 	int result = 0;
+	int One = one;
+	int N = n;
+	int Two = two;
+	int Three = three;
 
 	result = result + permu(N, one, two, three);//1로만 되있을때
-												//1을 2으로 바꾸면서 계산
+
+	//1을 2으로 바꾸면서 계산
 	while (one >= 2)
 	{
 		one -= 2;
 		two++;
-		N--;
-		result = result + permu(N, one, two, three);
+		n--;
+		result = result + permu(n, one, two, three);
 	}
 	//1을 3으로 바꾸면서 계산
-	while (one >= 3)
+	while (One >= 3)
 	{
-		one -= 3;
-		three++;
+		One -= 3;
+		Three++;
 		N -= 2;
-		result = result + permu(N, one, two, three);
+		result = result + permu(N, One, Two, Three);
 	}
 
 	return result;
@@ -50,7 +56,7 @@ int socket(int N, int one, int two, int three) {//처리하는 함수
 
 int carculration(int Q, int R) {// 되풀이 되는 과정을 나타냈습니다.
 	int result = 0;
-	while (Q > 0)
+	while (Q >= 0)
 	{
 		result = result + socket((Q * 2) + R, R, Q, Q);
 		Q--;
@@ -73,11 +79,11 @@ void main() {
 
 		q = input / 5;
 		r = input % 5;
-		
-		carculration(q, r);
+
+		result = carculration(q, r);
 
 		printf("결과: %d\n", result);
 
 	}
-	
+
 }
